29813dc956fb959d46076414c36a1f1fb569a5ac
714303584@qq.com <714303584@qq.com>
2016-08-26 19:21:59 +0800

    pom update!

---
+++ src/test/java/com/ifreeshare/spider/LuceneTest.java
@@ -0,0 +1,77 @@
+package com.ifreeshare.spider;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+public class LuceneTest {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		Analyzer ikAnalyzer = new IKAnalyzer(true);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String value = "可以看到src目录下有三个配置文件，分别为扩展字典文件ext.dic，停止词字典文件stopwprd.dic和配置文件IKAnalyzer.cfg.xml。"
+//				+ "配置文件IKAnalyzer.cfg.xml为配置扩展字典文件和停止词字典文件路径。IKAnalyzer.cfg.xml文件默认放置在classpath的根目录下，可以修改源码来改变该文件位置。";
+//		
+//		
+//		
+//		IKSegmenter ik = new IKSegmenter(new StringReader(value), true);
+//		
+//		Lexeme workd = null;
+//		
+//		try {
+//			while ((workd = ik.next()) != null) {
+//				
+//				System.out.println(workd.getLexemeText());
+//				
+//				
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+		
+		
+	}
+
+}
