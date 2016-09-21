bbf2743b12adcbc52b9067992191b08763fe9e8c
714303584@qq.com <714303584@qq.com>
2016-09-21 17:33:04 +0800

    The core object of the superclass

---
+++ src/main/java/com/ifreeshare/spider/core/CoreBase.java
@@ -0,0 +1,75 @@
+package com.ifreeshare.spider.core;
+
+/**
+ * @author zhuss
+ * @date 2016年9月19日下午5:26:20
+ */
+public class CoreBase {
+	
+	/**
+	 * redis  key address
+	 * key:md5
+	 * value: uuid of image
+	 */
+	public static final String MD5_UUID_IMAGE = "md5_uuid_image_ifreeshare_com";
+
+	public static final String SHA1_UUID_IMAGE = "sha1_uuid_image_ifreeshare_com";
+
+	public static final String SHA512_UUID_IMAGE = "sha512_uuid_image_ifreeshare_com";
+	
+	public static final String UUID_MD5_SHA1_SHA512_IMAGES_KEY = "uuid_md5_sha1_sha512_images_info_ifreeshare_com";
+	
+	public static final String MD5_SHA1_SHA512_EXIST_IMAGES_KEY = "md5_sha1_sha512_exist_image_key_ifreeshare_com";
+	
+	public static final String UUID = "uuid";
+	
+	public static final String MD5 = "md5";
+
+	public static final String SHA512 = "sha512";
+	public static final String SHA1 = "sha1";
+	
+	public static final String RESOLUTION = "resolution";
+
+	public static final String FILE_SIZE = "fileSize";
+	
+	public static final String SOURCE_ADDRESS = "sourceAddress";
+	
+	public static final String HTML_TITLE = "title";
+	
+	public static final String HTML_KEYWORDS = "keywords";
+	
+	public static final String HTML_DESCRIPTION = "description";
+	
+	public static final String Content_Type = "Content-Type";
+	
+	public static final String  TEXT_HTML = "text/html";
+	
+	public static final String  OBJECTS = "objects";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
