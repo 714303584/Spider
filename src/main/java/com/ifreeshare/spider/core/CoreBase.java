e4229beb65f9a114616e5659a59c5955c715ce49
714303584@qq.com <714303584@qq.com>
2016-10-10 19:06:43 +0800

    Add Some Key For Compressed File.

--- src/main/java/com/ifreeshare/spider/core/CoreBase.java
+++ src/main/java/com/ifreeshare/spider/core/CoreBase.java
@@ -15,6 +15,7 @@ public class CoreBase {
 
 	public static final String SHA512_UUID_IMAGE = "sha512_uuid_image_ifreeshare_com";
 	
+	//Image Storage Address  File Type :jpg,jpeg, png, icon........ Picture  Photo
 	public static final String UUID_MD5_SHA1_SHA512_IMAGES_KEY = "uuid_md5_sha1_sha512_images_info_ifreeshare_com";
 	
 	public static final String MD5_SHA1_SHA512_EXIST_IMAGES_KEY = "md5_sha1_sha512_exist_image_key_ifreeshare_com";
@@ -29,9 +30,23 @@ public class CoreBase {
 
 	public static final String SHA512_UUID_FILE = "sha512_uuid_file_ifreeshare_com";
 	
+	//File Storage Address  File Type :pdf ,doc , exel........ Document
 	public static final String UUID_MD5_SHA1_SHA512_FILE_KEY = "uuid_md5_sha1_sha512_file_info_ifreeshare_com";
 	
 	public static final String MD5_SHA1_SHA512_EXIST_FILE_KEY = "md5_sha1_sha512_exist_file_key_ifreeshare_com";
+	
+	/**
+	 * redis key for compressed file
+	 */
+	public static final String MD5_UUID_COMPRESSED_FILE = "md5_uuid_compressed_file_ifreeshare_com";
+
+	public static final String SHA1_UUID_COMPRESSED_FILE = "sha1_uuid_compressed_file_ifreeshare_com";
+
+	public static final String SHA512_UUID_COMPRESSED_FILE = "sha512_uuid_compressed_file_ifreeshare_com";
+	
+	//Compressed File Storage Address  File Type :rar ,zip, tar,tar.gz........ Compressed
+	public static final String UUID_MD5_SHA1_SHA512_COMPRESSED_FILE_KEY = "uuid_md5_sha1_sha512_compressed_file_info_ifreeshare_com";
+	public static final String MD5_SHA1_SHA512_EXIST_COMPRESSED_FILE_KEY = "md5_sha1_sha512_exist_compressed_file_key_ifreeshare_com";
 	
 	
 	public static final String DOT = ".";
