f665b9e98b20c9e10d25b711785a3c6385e0afad
714303584@qq.com <714303584@qq.com>
2016-11-03 19:01:34 +0800

    ErrorCode And ErrorMessage

---
+++ src/main/java/com/ifreeshare/spider/core/ErrorBase.java
@@ -0,0 +1,39 @@
+package com.ifreeshare.spider.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class is used to define the error code。
+ * At the beginning of the 1 is a request parameter error 
+ * @author zhuss
+ * @date 2016-10-28 - 3:57:29
+ */
+public class ErrorBase {
+	
+	//Input parameter format  error code 
+	public static final int DATA_I_TYPE_ERROR = 10001;
+	
+	//Output parameter format  error code 
+	public static final int DATA_O_TYPE_ERROR = 10002;
+	
+	//ErrorCode mapping Message
+	public static Map<Integer, String> codeAndMessage = new HashMap<Integer, String>();
+	
+	static{
+		codeAndMessage.put(DATA_I_TYPE_ERROR, "This parameter format is not supported ");
+
+		codeAndMessage.put(DATA_O_TYPE_ERROR, "This parameter format is not supported ");
+	}
+	
+
+	/**
+	 * Obtain error information by error code 
+	 * @param errorCode
+	 * @return ErrorMessage
+	 */
+	public static String getErrorMesage(int errorCode) {
+		return codeAndMessage.get(errorCode);
+	}
+
+}
