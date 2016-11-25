e79dfa3f1331ff59ee21f57c627375ab571a9c4e
714303584@qq.com <714303584@qq.com>
2016-11-25 19:53:13 +0800

    images

---
+++ src/test/java/com/ifreeshare/spider/CharSetCovert.java
@@ -0,0 +1,19 @@
+package com.ifreeshare.spider;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+
+
+public class CharSetCovert {
+	
+	public static void main(String[] args) {
+        String str="字符串编码转换";
+        
+        
+        
+        
+    }
+
+}
