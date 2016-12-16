687d0ec8ae3b61075c45cc25b7a117af425ebf94
714303584@qq.com <714303584@qq.com>
2016-12-16 11:20:09 +0800

    Add some notes

--- src/main/java/com/ifreeshare/spider/http/parse/BaseParser.java
+++ src/main/java/com/ifreeshare/spider/http/parse/BaseParser.java
@@ -14,15 +14,24 @@ import org.jsoup.select.Elements;
 import com.ifreeshare.spider.http.HttpUtil;
 import com.ifreeshare.spider.log.Log;
 
+/**
+ * Analysis of web pages 
+ * The use of the tool is JSOUP 
+ * @author zhuss
+ */
 public class BaseParser implements HtmlParser {
 	
 	protected static  Logger logger  = Log.register(BaseParser.class.getName());
 	
+	public static final String KEYWORD_SEPARATOR = ",";
 	
 	public static final String A = "a";
 	
 	public static final String META= "meta";
 
+	/**
+	 * Gets the title of the web page 
+	 */
 	@Override
 	public String getTitle(Document doc) {
 		String title = null;
@@ -31,16 +40,25 @@ public class BaseParser implements HtmlParser {
 		return title;
 	}
 
+	/**
+	 * Get the keyword for the web page 
+	 */
 	@Override
 	public String getKeywords(Document doc) {
 		return doc.head().select("meta[name=keywords]").attr("content");
 	}
 
+	/**
+	 * Get a description of the web page 
+	 */
 	@Override
 	public String getDescription(Document doc) {
 		return doc.head().select("meta[name=description]").attr("content");
 	}
 
+	/**
+	 * Gets the hyperlink to the web page 
+	 */
 	@Override
 	public Elements getLinks(Document doc) {
 		return doc.getElementsByTag(A);
@@ -51,6 +69,9 @@ public class BaseParser implements HtmlParser {
 		return null;
 	}
 
+	/**
+	 * Access to web pages 
+	 */
 	@Override
 	public Set<JsonObject> getLinkValue(Document doc) {
 		HashSet<JsonObject> results = new HashSet<JsonObject>();
@@ -78,9 +99,39 @@ public class BaseParser implements HtmlParser {
 		return results;
 	}
 	
+	
+	/**
+	 * Verify the legitimacy of the connection 
+	 * @param link
+	 * @param value
+	 * @return
+	 */
 	public boolean validatUrl(String link, String value){
 		return link.startsWith("http") && !"#".equals(value) && value != null && value.trim().length() > 0;
 		
+	}
+	
+	/**
+	 * Remove duplicate keywords 
+	 * Use a comma to separate
+	 * @param key
+	 * @return
+	 */
+	public static  String keywordDeWeight(String key){
+		String[] keys = key.toLowerCase().split(BaseParser.KEYWORD_SEPARATOR);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < keys.length; i++) {
+			String one = keys[i].trim();
+			if(sb.indexOf(one) > -1 || one.length() == 0){
+				continue;
+			}
+			if(i == (keys.length - 1)){
+				sb.append(one);
+			}else{
+				sb.append(one).append(BaseParser.KEYWORD_SEPARATOR);
+			}
+		}
+		return sb.toString();
 	}
 
 	
