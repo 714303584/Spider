4d60e80a82725f98af9a58f9a3a337052e0786f8
714303584@qq.com <714303584@qq.com>
2017-03-08 15:35:37 +0800

    Add entity tags

---
+++ src/main/java/com/ifreeshare/spider/http/server/page/Tags.java
@@ -0,0 +1,104 @@
+package com.ifreeshare.spider.http.server.page;
+
+public class Tags {
+	
+	private String id = "";
+	
+	private String name  = "";
+	
+	private String parent  = "";
+	
+	private String enkeywords = "";
+	
+	private String chkeywords  = "";
+
+	private String tag = "";
+	
+	private String keywords = "";
+	
+	private String title = "";
+	
+	private String description = "";
+	
+	private int status = 0;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getParent() {
+		return parent;
+	}
+
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+
+	public String getEnkeywords() {
+		return enkeywords;
+	}
+
+	public void setEnkeywords(String enkeywords) {
+		this.enkeywords = enkeywords;
+	}
+
+	public String getChkeywords() {
+		return chkeywords;
+	}
+
+	public void setChkeywords(String chkeywords) {
+		this.chkeywords = chkeywords;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+}
